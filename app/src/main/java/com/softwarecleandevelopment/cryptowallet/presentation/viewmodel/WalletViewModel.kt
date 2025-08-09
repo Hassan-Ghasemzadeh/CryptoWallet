1e8d4a805e0be0fc04a0cfd717539fc3ddb005b9
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-09 18:07:31 +0330

    [feat] : wallet viewmodel

---
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/presentation/viewmodel/WalletViewModel.kt
@@ -0,0 +1,30 @@
+package com.softwarecleandevelopment.cryptowallet.presentation.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import androidx.compose.runtime.State
+import androidx.lifecycle.viewModelScope
+import com.softwarecleandevelopment.cryptowallet.data.WalletRepositoryImpl
+import com.softwarecleandevelopment.cryptowallet.domain.GenerateWalletUseCase
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class WalletViewModel @Inject constructor(
+    private val walletRepository: WalletRepositoryImpl
+) : ViewModel() {
+    private var _mnemonic = mutableStateOf("")
+    val mnemonic: State<String> = _mnemonic
+    private var _address = mutableStateOf("")
+    val address: State<String> = _address
+
+    fun generateWallet() {
+        viewModelScope.launch {
+            val generateWalletUseCase = GenerateWalletUseCase(repository = walletRepository)
+            val wallet = generateWalletUseCase()
+            _mnemonic.value = wallet.mnemonic
+            _address.value = wallet.address
+        }
+    }
+}

887afa49c7e0292a91d7eb1a7c1371d9e345b1ee
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-09 18:08:35 +0330

    [feat] : dagger-hilt application

---
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/CryptoWallet.kt
@@ -0,0 +1,7 @@
+package com.softwarecleandevelopment.cryptowallet
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoWallet : Application() {}

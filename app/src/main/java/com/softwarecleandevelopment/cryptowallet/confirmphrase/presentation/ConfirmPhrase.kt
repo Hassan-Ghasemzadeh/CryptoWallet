46d71ed9b7695c7c38c464d6a01401567a0e6171
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-11 14:18:52 +0330

    [Feat] : ConfirmPhrase initial config

---
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/confirmphrase/presentation/ConfirmPhrase.kt
@@ -0,0 +1,3 @@
+package com.softwarecleandevelopment.cryptowallet.confirmphrase.presentation
+
+
---
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/confirmphrase/presentation/viewmodel/ConfirmPhraseViewModel.kt
@@ -0,0 +1,10 @@
+package com.softwarecleandevelopment.cryptowallet.confirmphrase.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ConfirmPhraseViewModel @Inject constructor() : ViewModel() {
+
+}

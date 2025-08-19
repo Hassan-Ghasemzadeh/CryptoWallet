ca9ca89c8c33f900c2506e52edde8ca02a2e4a55
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-19 15:36:34 +0330

    [refactor] : made code cleaner

--- app/src/main/java/com/softwarecleandevelopment/cryptowallet/recoveryphrase/data/WalletRepositoryImpl.kt
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/recoveryphrase/data/WalletRepositoryImpl.kt
@@ -4,10 +4,12 @@ import android.util.Log
 import com.softwarecleandevelopment.cryptowallet.recoveryphrase.domain.Wallet
 import com.softwarecleandevelopment.cryptowallet.recoveryphrase.domain.WalletRepository
 import org.kethereum.DEFAULT_ETHEREUM_BIP44_PATH
+import org.kethereum.bip32.model.ExtendedKey
 import org.kethereum.bip32.model.Seed
 import org.kethereum.bip32.toKey
 import org.kethereum.bip39.dirtyPhraseToMnemonicWords
 import org.kethereum.bip39.generateMnemonic
+import org.kethereum.bip39.model.MnemonicWords
 import org.kethereum.bip39.toSeed
 import org.kethereum.bip39.wordlists.WORDLIST_ENGLISH
 import org.kethereum.crypto.toAddress
@@ -19,15 +21,25 @@ import javax.inject.Singleton
 class WalletRepositoryImpl @Inject constructor() : WalletRepository {
     override suspend fun generateWallet(): Wallet {
         return runCatching {
-            val mnemonic = generateMnemonic(wordList = WORDLIST_ENGLISH)
-            val mnemonicWords = dirtyPhraseToMnemonicWords(mnemonic)
-            val seed = mnemonicWords.toSeed()
-            val masterKey = seed.toKey(DEFAULT_ETHEREUM_BIP44_PATH)
+            val mnemonicWords = generateMnemonicWords()
+            val masterKey = generateMasterKey(mnemonicWords = mnemonicWords)
             Wallet(mnemonicWords.words.joinToString(" "), masterKey.keyPair.privateKey.toString())
         }.getOrElse { exception ->
             Log.d("generateWallet", "Error Message: ${exception.message}", exception)
             Wallet("", "")
         }
+    }
+
+    private fun generateMnemonicWords(): MnemonicWords {
+        val mnemonic = generateMnemonic(wordList = WORDLIST_ENGLISH)
+        val mnemonicWords = dirtyPhraseToMnemonicWords(mnemonic)
+        return mnemonicWords
+    }
+
+    private fun generateMasterKey(mnemonicWords: MnemonicWords): ExtendedKey {
+        val seed = mnemonicWords.toSeed()
+        val masterKey = seed.toKey(DEFAULT_ETHEREUM_BIP44_PATH)
+        return masterKey
     }
 
     private fun generateCoinAddress(seed: Seed): String {

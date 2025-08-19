ff1015e22446ae1d4f5fadf2e42f3e454ad10311
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-19 18:44:59 +0330

    [Feat] : standard class for crypto coins

--- core/src/main/java/com/softwarecleandevelopment/core/crypto/BlockchainService.kt
+++ core/src/main/java/com/softwarecleandevelopment/core/crypto/BlockchainService.kt
@@ -5,4 +5,6 @@ import java.math.BigDecimal
 interface BlockchainService {
     suspend fun generateAddress()
     suspend fun getBalance(address: String): BigDecimal
+    suspend fun sendTransaction(from: String, to: String, amount: BigDecimal)
+    suspend fun getTransactions()
 }

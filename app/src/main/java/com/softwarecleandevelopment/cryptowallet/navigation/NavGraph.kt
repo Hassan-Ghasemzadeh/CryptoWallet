52eaa7590fb934ca4c129b048505341edd91dc50
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-21 11:57:01 +0330

    [Feat] : we put defined graph in the nav graph

--- app/src/main/java/com/softwarecleandevelopment/cryptowallet/navigation/NavGraph.kt
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/navigation/NavGraph.kt
@@ -1,55 +1,17 @@
 package com.softwarecleandevelopment.cryptowallet.navigation
 
-import com.softwarecleandevelopment.cryptowallet.confirmphrase.presentation.ConfirmPhraseScreen
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.softwarecleandevelopment.cryptowallet.landing.LandingScreen
-import com.softwarecleandevelopment.cryptowallet.recoveryphrase.presentation.RecoveryPhraseScreen
-import com.softwarecleandevelopment.cryptowallet.useragreement.presentation.UserAgreementScreen
+import com.softwarecleandevelopment.cryptowallet.navigation.NavHostExtension.createWalletGraph
 
 @Composable
 fun CryptoWalletNavGraph() {
     val navController = rememberNavController()
 
     NavHost(
-        navController = navController, startDestination = AppScreens.LandingScreen.route,
+        navController = navController, startDestination = AppGraph.CreateWalletGraph.graph,
     ) {
-
-        composable(route = AppScreens.LandingScreen.route) {
-            LandingScreen(
-                onCreateWalletClicked = {
-                    navController.navigate(AppScreens.UserAgreementScreen.route)
-                }
-            )
-        }
-        composable(route = AppScreens.UserAgreementScreen.route) {
-            UserAgreementScreen(
-                onContinueClicked = {
-                    navController.navigate(AppScreens.RecoveryPhraseScreen.route)
-                },
-                onNavigateBack = {
-                    navController.popBackStack()
-                }
-            )
-        }
-        composable(route = AppScreens.RecoveryPhraseScreen.route) {
-            RecoveryPhraseScreen(
-                onNavigateBack = {
-                    navController.popBackStack()
-                },
-                onContinueClicked = { mnemonic ->
-                    navController.navigate(AppScreens.ConfirmPhraseScreen.route)
-                }
-            )
-        }
-        composable(route = AppScreens.ConfirmPhraseScreen.route) {
-            ConfirmPhraseScreen(
-                onNavigateBack = {
-                    navController.popBackStack()
-                }
-            )
-        }
+        createWalletGraph(navController = navController)
     }
 }

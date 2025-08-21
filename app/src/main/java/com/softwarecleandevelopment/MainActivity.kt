5c0f06c8b7833efbbdb3af001ad467a4d4bbf7e2
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-21 12:38:10 +0330

    [refactor] :

--- app/src/main/java/com/softwarecleandevelopment/MainActivity.kt
+++ app/src/main/java/com/softwarecleandevelopment/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Initialize the observer
         enableEdgeToEdge()
         setContent {
             CryptoWalletTheme {

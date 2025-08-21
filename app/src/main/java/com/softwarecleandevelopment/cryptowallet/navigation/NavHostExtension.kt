6bd1d25fb0c6ab555304a50050c2c9cceb9eaa13
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-21 11:54:47 +0330

    [Feat] : nav host extension for every feature to put their screens composable their

---
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/navigation/NavHostExtension.kt
@@ -0,0 +1,55 @@
+package com.softwarecleandevelopment.cryptowallet.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.presentation.ConfirmPhraseScreen
+import com.softwarecleandevelopment.cryptowallet.landing.LandingScreen
+import com.softwarecleandevelopment.cryptowallet.recoveryphrase.presentation.RecoveryPhraseScreen
+import com.softwarecleandevelopment.cryptowallet.useragreement.presentation.UserAgreementScreen
+
+object NavHostExtension {
+    fun NavGraphBuilder.createWalletGraph(navController: NavController) {
+        navigation(
+            startDestination = CreateWalletScreens.LandingScreen.route,
+            route = AppGraph.CreateWalletGraph.graph
+        ) {
+
+            composable(route = CreateWalletScreens.LandingScreen.route) {
+                LandingScreen(
+                    onCreateWalletClicked = {
+                        navController.navigate(CreateWalletScreens.UserAgreementScreen.route)
+                    }
+                )
+            }
+            composable(route = CreateWalletScreens.UserAgreementScreen.route) {
+                UserAgreementScreen(
+                    onContinueClicked = {
+                        navController.navigate(CreateWalletScreens.RecoveryPhraseScreen.route)
+                    },
+                    onNavigateBack = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+            composable(route = CreateWalletScreens.RecoveryPhraseScreen.route) {
+                RecoveryPhraseScreen(
+                    onNavigateBack = {
+                        navController.popBackStack()
+                    },
+                    onContinueClicked = { mnemonic ->
+                        navController.navigate(CreateWalletScreens.ConfirmPhraseScreen.route)
+                    }
+                )
+            }
+            composable(route = CreateWalletScreens.ConfirmPhraseScreen.route) {
+                ConfirmPhraseScreen(
+                    onNavigateBack = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+    }
+}

5f035e6702d9ef26c3411da7be2a4ebefb0651b9
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-23 10:18:42 +0330

    [docs] : dependency to encrypt and decrypt room database fields added

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -75,4 +75,6 @@ dependencies {
     implementation(libs.androidx.datastore)
     //crypto tink
     implementation(libs.cryptoTink)
+    //bouncyCastle
+    implementation(libs.bouncyCastle.bcprov)
 }

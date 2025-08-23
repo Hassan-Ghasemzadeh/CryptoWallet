4515dd25234916fb362814412da119b0bafce725
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-23 10:22:50 +0330

    [recover]

---
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/recoveryphrase/domain/GeneratePhraseUseCase.kt
@@ -0,0 +1,11 @@
+package com.softwarecleandevelopment.cryptowallet.recoveryphrase.domain
+
+import com.softwarecleandevelopment.core.common.UseCase
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.models.Derived
+
+class GeneratePhraseUseCase(private val repository: PhraseRepository) : UseCase<Derived, Unit>() {
+    override suspend fun invoke(params: Unit): Derived {
+        return repository.generateWallet()
+    }
+
+}

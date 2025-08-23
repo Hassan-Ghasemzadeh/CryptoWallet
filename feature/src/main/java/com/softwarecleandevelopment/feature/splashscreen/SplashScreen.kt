677adbf560e74c63370b0fa0f33f0a8d34871680
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-23 17:55:50 +0330

    [feat] : splash screen ui improved

--- feature/src/main/java/com/softwarecleandevelopment/feature/splashscreen/SplashScreen.kt
+++ feature/src/main/java/com/softwarecleandevelopment/feature/splashscreen/SplashScreen.kt
@@ -2,7 +2,9 @@ package com.softwarecleandevelopment.feature.splashscreen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -37,10 +39,14 @@ fun SplashScreen(
     }
 
     // Your splash screen UI (e.g., logo, loading indicator)
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        CircularProgressIndicator()
+    Scaffold { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
     }
 }

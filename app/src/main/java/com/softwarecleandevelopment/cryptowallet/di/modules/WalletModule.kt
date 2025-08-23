c594dc655173a5245b60a4e9d54cddbfb7972292
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-23 10:31:25 +0330

    [feat] : provides room classes injection

--- app/src/main/java/com/softwarecleandevelopment/cryptowallet/di/modules/WalletModule.kt
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/di/modules/WalletModule.kt
@@ -1,11 +1,19 @@
 package com.softwarecleandevelopment.cryptowallet.di.modules
 
+import android.content.Context
+import androidx.room.Room
+import com.softwarecleandevelopment.core.crypto.CryptoStore
 import com.softwarecleandevelopment.core.database.WalletSecureStorage
-import com.softwarecleandevelopment.cryptowallet.recoveryphrase.data.WalletRepositoryImpl
-import com.softwarecleandevelopment.cryptowallet.recoveryphrase.domain.WalletRepository
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.database.WalletDatabase
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.source.WalletRepositoryImpl
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.models.WalletDao
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.domain.repository.WalletRepository
+import com.softwarecleandevelopment.cryptowallet.recoveryphrase.data.PhraseRepositoryImpl
+import com.softwarecleandevelopment.cryptowallet.recoveryphrase.domain.PhraseRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,7 +23,30 @@ object WalletModule {
 
     @Provides
     @Singleton
-    fun provideUserRepository(storage: WalletSecureStorage): WalletRepository {
-        return WalletRepositoryImpl(storage)
+    fun provideUserRepository(storage: WalletSecureStorage): PhraseRepository {
+        return PhraseRepositoryImpl(storage)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): WalletDatabase {
+        return Room.databaseBuilder(
+            context,
+            WalletDatabase::class.java,
+            "wallet_database",
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDao(walletDatabase: WalletDatabase): WalletDao {
+        return walletDatabase.walletDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideWalletRepository(dao: WalletDao, cryptoStore: CryptoStore): WalletRepository {
+        return WalletRepositoryImpl(dao, cryptoStore = cryptoStore)
+
     }
 }

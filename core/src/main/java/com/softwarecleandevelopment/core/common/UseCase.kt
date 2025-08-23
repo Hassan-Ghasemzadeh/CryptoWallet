2d9da2d3976dd30ed6b340ca5c3b9647bdd4e1c3
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-23 10:25:06 +0330

    [refactor] : changed interface to abstract class

--- core/src/main/java/com/softwarecleandevelopment/core/common/UseCase.kt
+++ core/src/main/java/com/softwarecleandevelopment/core/common/UseCase.kt
@@ -1,5 +1,5 @@
 package com.softwarecleandevelopment.core.common
 
-interface UseCase<out T, in P> {
-    suspend operator fun invoke(params: P): T
+abstract class UseCase<out T, in P> {
+    abstract suspend operator fun invoke(params: P): T
 }

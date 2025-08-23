4c29bca2a819fd7d032d4947fa9544b49f8286db
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-23 10:23:26 +0330

    [feat] : create wallet usecase

---
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/confirmphrase/domain/usecases/CreateWalletUseCase.kt
@@ -0,0 +1,14 @@
+package com.softwarecleandevelopment.cryptowallet.confirmphrase.domain.usecases
+
+import com.softwarecleandevelopment.core.common.UseCase
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.models.Derived
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.models.Result
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.domain.repository.WalletRepository
+import javax.inject.Inject
+
+class CreateWalletUseCase @Inject constructor(val repository: WalletRepository) :
+    UseCase<Result<Unit>, Derived>() {
+    override suspend fun invoke(params: Derived): Result<Unit> {
+        return repository.createNewWallet(derived = params)
+    }
+}

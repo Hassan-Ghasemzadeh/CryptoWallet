d93a5518a3f377ebb911ca535ef80f1f9f34588d
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-23 10:30:29 +0330

    [feat] : our database's table to define the id,name,etc

---
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/confirmphrase/data/models/WalletEntity.kt
@@ -0,0 +1,19 @@
+package com.softwarecleandevelopment.cryptowallet.confirmphrase.data.models
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "wallets", indices = [Index(value = ["chain", "address"], unique = true)]
+)
+data class WalletEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String = "Wallet #$id",
+    val chain: String,
+    val address: String,
+    val publicKeyHex: String?,
+    val privateKey: String,
+    val mnemonic: String,
+    val createdAt: Long = System.currentTimeMillis()
+)

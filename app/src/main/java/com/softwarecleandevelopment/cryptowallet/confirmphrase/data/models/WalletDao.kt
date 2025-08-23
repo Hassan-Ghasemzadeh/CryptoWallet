07191a8ec0cd0aac7abdbb623a808ea62b974380
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-23 10:29:08 +0330

    [feat] :  room interface class to handle database query

---
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/confirmphrase/data/models/WalletDao.kt
@@ -0,0 +1,17 @@
+package com.softwarecleandevelopment.cryptowallet.confirmphrase.data.models
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WalletDao {
+    @Insert(onConflict = OnConflictStrategy.Companion.ABORT)
+    suspend fun insert(wallet: WalletEntity)
+
+    @Query("SELECT * FROM wallets ORDER BY createdAt DESC")
+    fun observeAll(): Flow<List<WalletEntity>>
+
+}

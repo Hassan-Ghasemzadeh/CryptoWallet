0bc311b8177a8a19341e6260093552b129fde01e
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-23 10:19:42 +0330

    [docs] : chains type like ethereum,bitcoin,bnb

---
+++ core/src/main/java/com/softwarecleandevelopment/core/crypto/ChainType.kt
@@ -0,0 +1,8 @@
+package com.softwarecleandevelopment.core.crypto
+
+enum class ChainType(val displayName: String) {
+    MULTI_COIN("Multi-coin"),
+    ETHEREUM("Ethereum"),
+    BITCOIN("Bitcoin"),
+    BNB("Bnb Smart chain"),
+}

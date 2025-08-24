a2c9b909a2459ed1f2277c47ee216745cd171e9d
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-24 10:38:49 +0330

    [docs] : room files moved to :core.database.room

---
+++ core/src/main/java/com/softwarecleandevelopment/core/di/modules/RoomModule.kt
@@ -0,0 +1,35 @@
+package com.softwarecleandevelopment.core.di.modules
+
+import android.content.Context
+import androidx.room.Room
+import com.softwarecleandevelopment.core.database.room.database.WalletDatabase
+import com.softwarecleandevelopment.core.database.room.models.WalletDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): WalletDatabase {
+        return Room.databaseBuilder(
+            context,
+            WalletDatabase::class.java,
+            "wallet_database",
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDao(walletDatabase: WalletDatabase): WalletDao {
+        return walletDatabase.walletDao()
+    }
+
+
+}

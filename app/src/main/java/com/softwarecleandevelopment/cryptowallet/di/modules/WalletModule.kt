7e3f122f1ef465b9e5281b8af027afcba3806544
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-25 13:22:10 +0330

    [Recover] : now dagger-hilt provide WalletDataSource.kt and PhraseDatasource.kt

--- app/src/main/java/com/softwarecleandevelopment/cryptowallet/di/modules/WalletModule.kt
+++ app/src/main/java/com/softwarecleandevelopment/cryptowallet/di/modules/WalletModule.kt
@@ -2,10 +2,14 @@ package com.softwarecleandevelopment.cryptowallet.di.modules
 
 import com.softwarecleandevelopment.core.crypto.security.CryptoStore
 import com.softwarecleandevelopment.core.database.seed_datastore.SecureSeedStorage
-import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.source.WalletRepositoryImpl
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.repository.WalletRepositoryImpl
 import com.softwarecleandevelopment.core.database.room.dao.WalletDao
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.datasource.WalletDataSource
+import com.softwarecleandevelopment.cryptowallet.confirmphrase.data.datasource.WalletDataSourceImpl
 import com.softwarecleandevelopment.cryptowallet.confirmphrase.domain.repository.WalletRepository
-import com.softwarecleandevelopment.cryptowallet.recoveryphrase.data.PhraseRepositoryImpl
+import com.softwarecleandevelopment.cryptowallet.recoveryphrase.data.datasource.PhraseDataSourceImpl
+import com.softwarecleandevelopment.cryptowallet.recoveryphrase.data.datasource.PhraseDatasource
+import com.softwarecleandevelopment.cryptowallet.recoveryphrase.data.repository.PhraseRepositoryImpl
 import com.softwarecleandevelopment.cryptowallet.recoveryphrase.domain.repository.PhraseRepository
 import dagger.Module
 import dagger.Provides
@@ -16,16 +20,26 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object WalletModule {
+    @Provides
+    @Singleton
+    fun providePhraseDataSource(storage: SecureSeedStorage): PhraseDatasource {
+        return PhraseDataSourceImpl(storage)
+    }
 
     @Provides
     @Singleton
-    fun provideUserRepository(storage: SecureSeedStorage): PhraseRepository {
-        return PhraseRepositoryImpl(storage)
+    fun providePhraseRepository(dataSourceImpl: PhraseDataSourceImpl): PhraseRepository {
+        return PhraseRepositoryImpl(dataSourceImpl)
     }
+
+    fun provideWalletDataSource(dao: WalletDao, cryptoStore: CryptoStore): WalletDataSource {
+        return WalletDataSourceImpl(dao, cryptoStore)
+    }
+
     @Provides
     @Singleton
-    fun provideWalletRepository(dao: WalletDao, cryptoStore: CryptoStore): WalletRepository {
-        return WalletRepositoryImpl(dao, cryptoStore = cryptoStore)
+    fun provideWalletRepository(walletDataSourceImpl: WalletDataSourceImpl): WalletRepository {
+        return WalletRepositoryImpl(walletDataSourceImpl)
 
     }
 }

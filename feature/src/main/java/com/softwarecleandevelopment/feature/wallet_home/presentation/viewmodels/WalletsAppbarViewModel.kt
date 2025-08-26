21ad4e26176c344f08e57c50d2fb003a030c05c0
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-26 14:24:57 +0330

    [Refactor] : app bar viewmodel moved package

--- feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/viewmodels/WalletsAppbarViewModel.kt
+++ feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/viewmodels/WalletsAppbarViewModel.kt
@@ -1,4 +1,4 @@
-package com.softwarecleandevelopment.feature.wallet_home.presentation
+package com.softwarecleandevelopment.feature.wallet_home.presentation.viewmodels
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf

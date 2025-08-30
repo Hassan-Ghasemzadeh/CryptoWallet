c175822075ca08ae42c6824807de55e751087165
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-08-30 12:41:30 +0330

    [docs] : hold information about crypto

---
+++ crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/ethereum/domain/models/CryptoInfo.kt
@@ -0,0 +1,11 @@
+package com.softwarecleandevelopment.crypto_chains.ethereum.domain.models
+
+data class CryptoInfo(
+    val id: String,
+    val symbol: String,
+    val name: String,
+    val iconRes: Int,
+    val priceUsd: Double = 0.0,
+    val changePrecent: Double = 0.0,
+    val balance: Double = 0.0,
+)

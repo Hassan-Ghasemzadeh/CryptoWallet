06359159a4da4d007874465929fa530c31bc4634
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-03 10:26:44 +0330

    [Recover] : changed fun return type and added Resource util to it

--- crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/ethereum/domain/repository/local/EthLocalRepository.kt
+++ crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/ethereum/domain/repository/local/EthLocalRepository.kt
@@ -1,10 +1,12 @@
 package com.softwarecleandevelopment.crypto_chains.ethereum.domain.repository.local
 
+import com.softwarecleandevelopment.core.common.utils.Resource
 import com.softwarecleandevelopment.core.crypto.repository.BlockchainService
+import kotlinx.coroutines.flow.Flow
 import java.math.BigDecimal
 
 interface EthLocalRepository : BlockchainService {
-    override suspend fun generateAddress()
+    override fun generateAddress(): Resource<Flow<String?>>
     override suspend fun sendTransaction(from: String, to: String, amount: BigDecimal)
     override suspend fun getTransaction()
 }

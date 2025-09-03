1b5a6b1f25af6186b9ac195804ffdea511ef9e5a
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-03 17:01:58 +0330

    [docs] : unused imports removed

--- feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/WalletHome.kt
+++ feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/WalletHome.kt
@@ -11,17 +11,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon

2942030e599ccaa212c9e7ff053a488a8ec7fb20
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-03 10:27:11 +0330

    [Recover] : added ability to generate address

---
+++ crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/ethereum/data/repository/local/EthLocalRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.softwarecleandevelopment.crypto_chains.ethereum.data.repository.local
+
+import com.softwarecleandevelopment.core.common.utils.Resource
+import com.softwarecleandevelopment.core.common.utils.safeFlowCall
+import com.softwarecleandevelopment.crypto_chains.ethereum.data.datasource.local.EthLocalDatasource
+import com.softwarecleandevelopment.crypto_chains.ethereum.domain.repository.local.EthLocalRepository
+import kotlinx.coroutines.flow.Flow
+import java.math.BigDecimal
+import javax.inject.Inject
+
+class EthLocalRepositoryImpl @Inject constructor(
+    private val datasource: EthLocalDatasource,
+) : EthLocalRepository {
+    override fun generateAddress(): Resource<Flow<String?>> {
+        return safeFlowCall { datasource.generateAddress() }
+    }
+
+    override suspend fun sendTransaction(
+        from: String, to: String, amount: BigDecimal
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTransaction() {
+        TODO("Not yet implemented")
+    }
+}

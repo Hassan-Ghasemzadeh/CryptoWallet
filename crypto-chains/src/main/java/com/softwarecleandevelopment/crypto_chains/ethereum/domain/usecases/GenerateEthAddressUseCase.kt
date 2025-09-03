0232b7aeb0c0dd4be4ea0228069e3fd0ba2cce6d
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-03 10:45:21 +0330

    [Feat] : eth generate address usecase added

---
+++ crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/ethereum/domain/usecases/GenerateEthAddressUseCase.kt
@@ -0,0 +1,15 @@
+package com.softwarecleandevelopment.crypto_chains.ethereum.domain.usecases
+
+import com.softwarecleandevelopment.core.common.utils.Resource
+import com.softwarecleandevelopment.core.common.utils.UseCase
+import com.softwarecleandevelopment.crypto_chains.ethereum.domain.repository.local.EthLocalRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GenerateEthAddressUseCase @Inject constructor(
+    private val repository: EthLocalRepository,
+) : UseCase<Resource<Flow<String?>>, Unit>() {
+    override suspend fun invoke(params: Unit): Resource<Flow<String?>> {
+        return repository.generateAddress()
+    }
+}

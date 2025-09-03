9566e0886f43082a3aa8e94e8e602092790cdacd
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-03 11:03:57 +0330

    [Feat] : provides repository and datasource in di

--- feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/viewmodels/WalletHomeViewModel.kt
+++ feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/viewmodels/WalletHomeViewModel.kt
@@ -8,33 +8,57 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.softwarecleandevelopment.core.common.utils.Resource
+import com.softwarecleandevelopment.crypto_chains.ethereum.data.model.initialCryptos
 import com.softwarecleandevelopment.crypto_chains.ethereum.domain.models.CryptoInfo
 import com.softwarecleandevelopment.crypto_chains.ethereum.domain.models.GetCryptoInfoEvent
+import com.softwarecleandevelopment.crypto_chains.ethereum.domain.usecases.GenerateEthAddressUseCase
 import com.softwarecleandevelopment.crypto_chains.ethereum.domain.usecases.GetCryptoInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@RequiresApi(Build.VERSION_CODES.O)
 @HiltViewModel
 class WalletHomeViewModel @Inject constructor(
-   private val getCryptoInfoUseCase: GetCryptoInfoUseCase,
+    private val getCryptoInfoUseCase: GetCryptoInfoUseCase,
+    private val generateEthAddressUseCase: GenerateEthAddressUseCase,
 ) : ViewModel() {
     private val _cryptos: MutableState<List<CryptoInfo>> = mutableStateOf(listOf())
     val cryptos: State<List<CryptoInfo>> = _cryptos
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun loadCryptoInfo(event: GetCryptoInfoEvent) {
+
+    init {
+        loadCryptoInfo()
+    }
+
+    fun loadCryptoInfo() {
         viewModelScope.launch {
-            val result = getCryptoInfoUseCase.invoke(event)
-            when (result) {
+            val generate = generateEthAddressUseCase.invoke(Unit)
+            when (generate) {
                 is Resource.Error -> {
-                    _cryptos.value = listOf()
+
                 }
 
-                is Resource.Success<Flow<List<CryptoInfo>>> -> {
-                    result.data.collectLatest { cryptoInfos ->
-                        _cryptos.value = cryptoInfos
+                is Resource.Success<Flow<String?>> -> {
+                    val result = getCryptoInfoUseCase.invoke(
+                        GetCryptoInfoEvent(
+                            initialCryptos,
+                            generate.data.first().toString()
+
+                        )
+                    )
+                    when (result) {
+                        is Resource.Error -> {
+                            _cryptos.value = listOf()
+                        }
+
+                        is Resource.Success<Flow<List<CryptoInfo>>> -> {
+                            result.data.collectLatest { cryptoInfos ->
+                                _cryptos.value = cryptoInfos
+                            }
+                        }
                     }
                 }
             }

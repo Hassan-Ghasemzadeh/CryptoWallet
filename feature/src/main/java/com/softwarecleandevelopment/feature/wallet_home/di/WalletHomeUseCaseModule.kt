39edaee08def114a1fe3333f932066eaec595cf0
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-04 11:33:27 +0330

    [Feat] : provides usecases in dagger-hilt

--- feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/di/WalletHomeUseCaseModule.kt
+++ feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/di/WalletHomeUseCaseModule.kt
@@ -1,10 +1,15 @@
 package com.softwarecleandevelopment.feature.wallet_home.di
 
+import android.content.Context
+import com.softwarecleandevelopment.feature.wallet_home.domain.usecases.CopyToClipboardUseCase
+import com.softwarecleandevelopment.feature.wallet_home.domain.usecases.GenerateQrBitmapUseCase
 import com.softwarecleandevelopment.feature.wallet_home.domain.usecases.GetActiveWalletUseCase
+import com.softwarecleandevelopment.feature.wallet_home.domain.usecases.ShareTextUseCase
 import com.softwarecleandevelopment.feature.wallets.domain.repository.WalletsRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,5 +21,26 @@ object WalletHomeUseCaseModule {
     @Singleton
     fun provideGetActiveWalletUseCase(walletsRepository: WalletsRepository): GetActiveWalletUseCase {
         return GetActiveWalletUseCase(walletsRepository)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideGenerateQrCodeUseCase(): GenerateQrBitmapUseCase {
+        return GenerateQrBitmapUseCase()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideCopyToClipBoardUseCase(@ApplicationContext context: Context): CopyToClipboardUseCase {
+        return CopyToClipboardUseCase(context)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideShareUseCase(@ApplicationContext context: Context): ShareTextUseCase {
+        return ShareTextUseCase(context)
     }
 }

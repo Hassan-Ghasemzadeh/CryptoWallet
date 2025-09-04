eadf805f8448ced8ecd698d8bad3baa8e1847fb3
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-04 11:35:57 +0330

    [Fix] : fixed a bug in copy yo clipboard

--- feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/ReceiveScreen.kt
+++ feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/ReceiveScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.IosShare
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material3.*

179826a0a05034594a99c8982165647455e8b3a2
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-07 14:19:31 +0330

    [Feat] : ability to paste first copied item in clipboard

--- feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/viewmodels/send/SendCoinViewModel.kt
+++ feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/viewmodels/send/SendCoinViewModel.kt
@@ -1,13 +1,18 @@
 package com.softwarecleandevelopment.feature.wallet_home.presentation.viewmodels.send
 
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 @HiltViewModel
-class SendCoinViewModel @Inject constructor() : ViewModel() {
+class SendCoinViewModel @Inject constructor(
+    @ApplicationContext val context: Context,
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(SendCoinUiState())
     val uiState = _uiState.asStateFlow()
@@ -24,4 +29,25 @@ class SendCoinViewModel @Inject constructor() : ViewModel() {
         // Example: Fetch balance and set it
         _uiState.value = _uiState.value.copy(amount = "1000")
     }
+
+    // A function to get text from the clipboard
+    fun getCopiedText(): String? {
+        val clipboardManager =
+            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+        if (clipboardManager.hasPrimaryClip()) {
+            val clipData = clipboardManager.primaryClip
+
+            if (clipData != null && clipData.itemCount > 0) {
+                val item = clipData.getItemAt(0)
+
+                val text = item.text
+
+                return text?.toString()
+            }
+        }
+
+        return null
+    }
+
 }

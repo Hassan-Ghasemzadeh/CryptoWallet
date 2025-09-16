e88f946544404351e63551c0ac86638153df59b8
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-16 14:53:57 +0330

    [Refactor] : web3j is added to dependency

--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.compose.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -76,6 +77,8 @@ dependencies {
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.view)
+    //web3j
+    implementation(libs.web3j.core)
 }
 
 

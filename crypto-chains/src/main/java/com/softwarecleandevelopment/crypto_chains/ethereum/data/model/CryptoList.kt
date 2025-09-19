9cc514873019858947a1b842ce1eb3f745382a74
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-19 14:05:49 +0330

    [Feat] : added new token info to List

--- crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/ethereum/data/model/CryptoList.kt
+++ crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/ethereum/data/model/CryptoList.kt
@@ -10,5 +10,23 @@ val initialCryptos = listOf<CryptoInfo>(
         symbol = "ETH",
         name = "Ethereum",
         iconRes = R.drawable.ic_eth
+    ),
+    CryptoInfo(
+        id = "bitcoin",
+        symbol = "BTC",
+        name = "Bitcoin",
+        iconRes = R.drawable.ic_btc
+    ),
+    CryptoInfo(
+        id = "doge",
+        symbol = "DOGE",
+        name = "Dogecoin",
+        iconRes = R.drawable.ic_doge
+    ),
+    CryptoInfo(
+        id = "tether",
+        symbol = "USDT",
+        name = "Tether",
+        iconRes = R.drawable.ic_usdt
     )
 )

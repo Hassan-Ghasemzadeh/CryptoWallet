23a4b6f323f0209d656ac9030bbb3b3a9a97e9b7
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-21 11:18:30 +0330

    [Refactor] : dagger-hilt module for crypto_info

---
+++ crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/crypto_info/di/CryptoInfoModule.kt
@@ -0,0 +1,39 @@
+package com.softwarecleandevelopment.crypto_chains.crypto_info.di
+
+import com.softwarecleandevelopment.crypto_chains.crypto_info.data.datasource.CryptoApi
+import com.softwarecleandevelopment.crypto_chains.crypto_info.data.datasource.CryptoInfoDataSourceImpl
+import com.softwarecleandevelopment.crypto_chains.crypto_info.data.datasource.CryptoInfoDatasource
+import com.softwarecleandevelopment.crypto_chains.crypto_info.data.repository.CryptoInfoRepositoryImpl
+import com.softwarecleandevelopment.crypto_chains.crypto_info.domain.repository.CryptoInfoRepository
+import com.softwarecleandevelopment.crypto_chains.ethereum.data.datasource.remote.EthRemoteDatasource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CryptoInfoModule {
+    @Provides
+    @Singleton
+    fun provideEthCryptoApi(retrofit: Retrofit): CryptoApi {
+        return retrofit.create(CryptoApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCryptoInfoDataSource(
+        api: CryptoApi,
+        ethRemoteDatasource: EthRemoteDatasource
+    ): CryptoInfoDatasource {
+        return CryptoInfoDataSourceImpl(api, ethRemoteDatasource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCryptoInfoRepository(datasource: CryptoInfoDatasource): CryptoInfoRepository {
+        return CryptoInfoRepositoryImpl(datasource = datasource)
+    }
+}

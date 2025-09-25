bbab59618d2f8cf4d7f50d2b88453dd98e830189
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-25 11:40:53 +0330

    [Refactor] : implements the BlockChainService class in the repository

--- crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/dogecoin/data/repository/DogeCoinRepositoryImpl.kt
+++ crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/dogecoin/data/repository/DogeCoinRepositoryImpl.kt
@@ -3,12 +3,13 @@ package com.softwarecleandevelopment.crypto_chains.dogecoin.data.repository
 import com.softwarecleandevelopment.core.crypto.models.AddressParams
 import com.softwarecleandevelopment.core.common.utils.Resource
 import com.softwarecleandevelopment.core.common.utils.safeCall
+import com.softwarecleandevelopment.core.crypto.repository.BlockchainService
 import com.softwarecleandevelopment.crypto_chains.dogecoin.data.datasource.DogeCoinDataSource
 import com.softwarecleandevelopment.crypto_chains.dogecoin.domian.repository.DogeCoinRepository
 import javax.inject.Inject
 
 class DogeCoinRepositoryImpl @Inject constructor(private val dataSource: DogeCoinDataSource) :
-    DogeCoinRepository {
+    DogeCoinRepository, BlockchainService<AddressParams> {
     override suspend fun generateAddress(
         params: AddressParams,
     ): Resource<String> {
--- crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/tether/data/repository/TetherRepositoryImpl.kt
+++ crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/tether/data/repository/TetherRepositoryImpl.kt
@@ -4,12 +4,13 @@ import com.softwarecleandevelopment.core.common.utils.Resource
 import com.softwarecleandevelopment.core.common.utils.safeCall
 import com.softwarecleandevelopment.crypto_chains.tether.data.datasource.TetherDataSource
 import com.softwarecleandevelopment.core.crypto.models.AddressParams
+import com.softwarecleandevelopment.core.crypto.repository.BlockchainService
 import com.softwarecleandevelopment.crypto_chains.tether.domain.repository.TetherRepository
 import javax.inject.Inject
 
 class TetherRepositoryImpl @Inject constructor(
     private val dataSource: TetherDataSource
-) : TetherRepository {
+) : TetherRepository, BlockchainService<AddressParams> {
     override suspend fun generateAddress(
         params: AddressParams
     ): Resource<String> {

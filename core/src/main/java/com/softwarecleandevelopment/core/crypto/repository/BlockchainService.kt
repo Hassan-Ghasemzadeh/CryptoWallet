02db94138ddf3b4484d5b30455956d3209ed589d
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-25 11:27:02 +0330

    [Refactor] : standard class for tokens in refactored

--- core/src/main/java/com/softwarecleandevelopment/core/crypto/repository/BlockchainService.kt
+++ core/src/main/java/com/softwarecleandevelopment/core/crypto/repository/BlockchainService.kt
@@ -1,11 +1,8 @@
 package com.softwarecleandevelopment.core.crypto.repository
 
 import com.softwarecleandevelopment.core.common.utils.Resource
-import kotlinx.coroutines.flow.Flow
-import java.math.BigDecimal
 
-interface BlockchainService {
-    fun generateAddress(): Resource<Flow<String?>>
-    suspend fun sendTransaction(from: String, to: String, amount: BigDecimal)
-    suspend fun getTransaction()
+
+interface BlockchainService<in P> {
+    suspend fun generateAddress(params: P): Resource<String>
 }

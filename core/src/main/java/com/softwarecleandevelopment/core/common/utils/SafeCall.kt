c184b18d4a3eca98a1a6c773c7af41a740201f9a
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-25 11:47:49 +0330

    [Refactor] : implements the BlockChainService class in the repository

--- core/src/main/java/com/softwarecleandevelopment/core/common/utils/SafeCall.kt
+++ core/src/main/java/com/softwarecleandevelopment/core/common/utils/SafeCall.kt
@@ -2,10 +2,7 @@ package com.softwarecleandevelopment.core.common.utils
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
 
 sealed class Resource<out T> {
     data class Success<out T>(val data: T) : Resource<T>()
@@ -32,28 +29,19 @@ suspend fun <T> safeCall(
 }
 
 fun <T> safeFlowCall(
-    // Input is a suspending function to fetch the data
-    call: suspend () -> T
-): Flow<Resource<T>> = flow {
-    // Emit Loading state immediately
-    emit(Resource.Loading)
-
-    try {
-        //Call the suspending function (often a Repository or DataSource call)
+    call: () -> Flow<T>
+): Resource<Flow<T>> {
+    return try {
+        Resource.Loading
         val result = call()
-
-        // Emit Success state with the data
-        emit(Resource.Success(result))
-
+        Resource.Success(result)
     } catch (e: Exception) {
         e.printStackTrace()
-
-        // Handle exceptions and emit Error state
         val errorMessage = when (e) {
-            is SocketTimeoutException -> "Connection timed out. Please try again."
-            is UnknownHostException -> "No internet connection. Please check your network."
+            is java.net.SocketTimeoutException -> "Connection timed out. Please try again."
+            is java.net.UnknownHostException -> "No internet connection. Please check your network."
             else -> "An unexpected error occurred."
         }
-        emit(Resource.Error(errorMessage, e))
+        Resource.Error(errorMessage, e)
     }
 }

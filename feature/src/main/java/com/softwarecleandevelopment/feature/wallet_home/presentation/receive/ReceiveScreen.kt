42289421b2f6d7a66965c849c2d79cdf32b733c9
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-27 10:49:35 +0330

    [Docs] : moved to receive screen package

--- feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/receive/ReceiveScreen.kt
+++ feature/src/main/java/com/softwarecleandevelopment/feature/wallet_home/presentation/receive/ReceiveScreen.kt
@@ -1,4 +1,4 @@
-package com.softwarecleandevelopment.feature.wallet_home.presentation
+package com.softwarecleandevelopment.feature.wallet_home.presentation.receive
 
 
 import android.graphics.Bitmap

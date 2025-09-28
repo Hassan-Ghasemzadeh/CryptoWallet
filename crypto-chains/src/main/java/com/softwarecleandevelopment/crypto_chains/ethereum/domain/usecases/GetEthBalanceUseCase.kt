f13396b3e275c470eaa355f8c671b42afe6572b2
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-09-28 11:21:13 +0330

    [Feat] : added ability to get eth balance

---
+++ crypto-chains/src/main/java/com/softwarecleandevelopment/crypto_chains/ethereum/domain/usecases/GetEthBalanceUseCase.kt
@@ -0,0 +1,13 @@
+package com.softwarecleandevelopment.crypto_chains.ethereum.domain.usecases
+
+import com.softwarecleandevelopment.core.common.utils.Constants
+import com.softwarecleandevelopment.core.common.utils.UseCase
+import com.softwarecleandevelopment.crypto_chains.ethereum.data.datasource.EthDatasource
+import javax.inject.Inject
+
+class GetEthBalanceUseCase @Inject constructor(private val datasource: EthDatasource) :
+    UseCase<Double, String>() {
+    override suspend fun invoke(params: String): Double {
+        return datasource.getEthBalance(Constants.rpcUrl, params)
+    }
+}

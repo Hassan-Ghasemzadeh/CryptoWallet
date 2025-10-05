33f53ac9d9fe0947f26a23aeafb42e09fed52bef
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-10-05 17:10:17 +0330

    [Refactor] : added afew field

--- core/src/main/java/com/softwarecleandevelopment/core/crypto/models/CoinInfo.kt
+++ core/src/main/java/com/softwarecleandevelopment/core/crypto/models/CoinInfo.kt
@@ -1,7 +1,8 @@
-package com.softwarecleandevelopment.crypto_chains.crypto_info.domain.model
+package com.softwarecleandevelopment.core.crypto.models
 
 import com.softwarecleandevelopment.core.common.utils.AddressGenerator
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 data class CoinInfo(
@@ -12,6 +13,7 @@ data class CoinInfo(
     val priceUsd: Double = 0.0,
     val changePrecent: Double = 0.0,
     val balance: Double = 0.0,
-    val generator: AddressGenerator,
+    @Transient
+    val generator: AddressGenerator? = null,
     val updatedAt: Long = System.currentTimeMillis()
 )

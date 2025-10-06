0582cda6691130e786814a90455705420effbafd
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-10-06 18:30:55 +0330

    [Feat,Fix] : made the class parcelable so it can be passed throw the saved handle state

--- core/src/main/java/com/softwarecleandevelopment/core/crypto/models/CoinInfo.kt
+++ core/src/main/java/com/softwarecleandevelopment/core/crypto/models/CoinInfo.kt
@@ -1,9 +1,13 @@
 package com.softwarecleandevelopment.core.crypto.models
 
+import android.os.Parcelable
 import com.softwarecleandevelopment.core.common.utils.AddressGenerator
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Serializable
 data class CoinInfo(
     val id: String,
@@ -13,7 +17,8 @@ data class CoinInfo(
     val priceUsd: Double = 0.0,
     val changePrecent: Double = 0.0,
     val balance: Double = 0.0,
+    @IgnoredOnParcel
     @Transient
     val generator: AddressGenerator? = null,
     val updatedAt: Long = System.currentTimeMillis()
-)
+) : Parcelable

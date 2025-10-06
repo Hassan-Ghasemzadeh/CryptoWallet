442c74cb9d27a091408538b84524e48c30fbcdf9
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-10-06 18:33:18 +0330

    [Feat,Fix] : object is removed and now screens are type of data object

--- feature/src/main/java/com/softwarecleandevelopment/feature/dashboard/navigation/HomeScreens.kt
+++ feature/src/main/java/com/softwarecleandevelopment/feature/dashboard/navigation/HomeScreens.kt
@@ -1,14 +1,16 @@
 package com.softwarecleandevelopment.feature.dashboard.navigation
 
 sealed class HomeScreens(val route: String) {
-    object SplashScreens : HomeScreens("splash_screen")
-    object DashboardScreen : HomeScreens("dashboard_screen")
-    object WalletsScreen : HomeScreens("wallets_screen")
-    object WalletDetailScreen : HomeScreens("wallet_detail_screen")
-    object WalletSecretPhraseScreen : HomeScreens("wallet_Secret_phrase_screen")
-    object ReceiveScreen : HomeScreens("receive_screen")
-    object ReceiveTokensScreen : HomeScreens("receive_tokens_screen")
-    object SendScreen : HomeScreens("Send_screen")
-    object SendTokensScreen : HomeScreens("send_tokens_screen")
+
+    data object SplashScreens : HomeScreens("splash_screen")
+    data object DashboardScreen : HomeScreens("dashboard_screen")
+    data object WalletsScreen : HomeScreens("wallets_screen")
+    data object WalletDetailScreen : HomeScreens("wallet_detail_screen")
+    data object WalletSecretPhraseScreen : HomeScreens("wallet_Secret_phrase_screen")
+    data object ReceiveScreen : HomeScreens("receive_screen")
+    data object ReceiveTokensScreen : HomeScreens("receive_tokens_screen")
+    data object SendScreen : HomeScreens("Send_screen")
+    data object SendTokensScreen : HomeScreens("send_tokens_screen")
+    data object CoinScreen : HomeScreens("coin_screen")
 
 }

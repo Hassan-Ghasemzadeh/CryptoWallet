55bbef5066ae399e432a67302785f308707fe24a
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-10-09 19:03:58 +0330

    [Feat] : added the datastore to dagger hilt

---
+++ core/src/main/java/com/softwarecleandevelopment/core/di/modules/SettingsModule.kt
@@ -0,0 +1,38 @@
+package com.softwarecleandevelopment.core.di.modules
+
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SettingsModule {
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    @Singleton
+    @Provides
+    @SettingsDataStore
+    fun provideSettingPreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            scope = scope,
+            produceFile = { context.preferencesDataStoreFile("setting_datastore") }
+        )
+    }
+}
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class SettingsDataStore

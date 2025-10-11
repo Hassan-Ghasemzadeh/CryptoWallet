c997d70ee649ce0b8eac0e94bf98447896fb8cfa
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-10-11 12:08:46 +0330

    [Refactor] : removed unused classes

--- core/src/main/java/com/softwarecleandevelopment/core/common/model/Settings.kt
+++
@@ -1,7 +0,0 @@
-package com.softwarecleandevelopment.core.common.model
-
-data class Settings(
-    val isNotificationsEnabled: Boolean,
-    val preferredCurrency: String,
-    val preferredLanguage: String
-)

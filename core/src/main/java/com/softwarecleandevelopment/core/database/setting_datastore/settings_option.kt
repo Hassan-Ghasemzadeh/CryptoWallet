af1f4e3f5d2b2e1ed54fed40d3c03652011916ec
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-10-11 12:09:56 +0330

    [Feat,Fix] : setting options like darkMode/Notification/language/currency

--- core/src/main/java/com/softwarecleandevelopment/core/database/setting_datastore/settings_option.kt
+++ core/src/main/java/com/softwarecleandevelopment/core/database/setting_datastore/settings_option.kt
@@ -5,4 +5,5 @@ sealed class SettingsOption(val name: String) {
     data class Notification(val isEnabled: Boolean) : SettingsOption("notification")
     data class Currency(val code: String) : SettingsOption("currency")
     data class Language(val language: String) : SettingsOption("language")
+    data class DarkMode(val isEnabled: Boolean) : SettingsOption("dark_mode")
 }
